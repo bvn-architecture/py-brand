99f2e3f62faa2ad58534dbfad96ebfebd56c4b5e
Ben Doherty <ben@notionparallax.co.uk>
2022-06-07 14:31:23 +1000

    expose things in the init

--- src/bvn_values/__init__.py
+++ src/bvn_values/__init__.py
@@ -1,4 +1,5 @@
 """BVN specific values and functions and things."""
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
-# from .values import get_bvn_brand_colours
+# importing these here makes them avaliabel to the dir command
+from .values import get_bvn_brand_colours, company_name
