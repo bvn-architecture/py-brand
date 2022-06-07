7720b77ccec1ddec5a24a36cf5c978ae607f0a64
Ben Doherty <ben@notionparallax.co.uk>
2022-06-07 14:09:52 +1000

    flit things

--- src/bvn/__main__.py
+++ src/bvn/__main__.py
@@ -1,19 +1,19 @@
-from colours import bvn_brand_colours
+from .values import get_bvn_brand_colours, company_name
 
 
 def say_hi():
     print(
-        """
+        f"""
         ______     ___   _ 
         | __ ) \   / / \ | |
         |  _ \\ \ / /|  \| |
         | |_) |\ V / | |\  |
         |____/  \_/  |_| \_|
                         
-       Brand colours, for now.
+       Brand colours, for {company_name}, for now.
     """
     )
-    for name, hex in bvn_brand_colours.items():
+    for name, hex in get_bvn_brand_colours().items():
         print(f"{name}: {hex}")
 
 
